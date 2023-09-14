f68874d80f7fdf87f73799a481ae0aeacf5e12f3
dhandyjoenathankreditplus <dhandy.kurnia@kpvendor.id>
2023-09-15 04:55:00 +0700

    add reporting automation

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+	reporter: 'cypress-mochawesome-reporter',
+	reporterOptions: {
+		charts: true,
+		reportPageTitle: 'custom-title',
+		embeddedScreenshots: true,
+		inlineAssets: true,
+		saveAllAttempts: false,
+	},
 	e2e: {
 		watchForFileChanges: false,
 		experimentalModifyObstructiveThirdPartyCode: true,
@@ -8,6 +16,7 @@ module.exports = defineConfig({
 
 		setupNodeEvents(on, config) {
 			// implement node event listeners here
+			require('cypress-mochawesome-reporter/plugin')(on);
 		},
 	},
 });
